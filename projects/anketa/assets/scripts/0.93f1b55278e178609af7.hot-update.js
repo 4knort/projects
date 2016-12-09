0921f0113731be87a62ce189e20b66ef7c7ba3d0
4knort <4knort@gmail.com>
2016-12-09 23:31:52 +0200

    Update 2016-12-09T21:31:39.604Z

---
+++ projects/anketa/assets/scripts/0.93f1b55278e178609af7.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate(0,{
+
+/***/ 6:
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.default = slider;
+	function slider() {
+		$(".form").ionRangeSlider({
+			grid: true,
+			values: ["one", "two", "three", "four"]
+		});
+	}
+
+/***/ }
+
+})
+//# sourceMappingURL=0.93f1b55278e178609af7.hot-update.js.map
